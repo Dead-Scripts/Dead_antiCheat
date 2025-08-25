fa86ef8388634b33b25a54928e0c067fa721f1eb
Dead-Scripts <btiede590@gmail.com>
2025-08-25 01:06:14 -0500

    update

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Dead'
+description "Dead's Official Anticheat"
+
+shared_script 'config.lua'
+
+client_scripts {
+    'Enumerators.lua',
+    'client.lua', 
+    'acloader.lua'
+}
+
+server_scripts {
+    'server.lua',
+}
